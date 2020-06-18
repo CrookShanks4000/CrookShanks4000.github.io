e6a0b01d5e900d5214f43e0310426ed03108a5eb
CrookShanks4000 <50306750+CrookShanks4000@users.noreply.github.com>
2020-06-18 15:52:28 -0500

    Update verifyregistration.js

--- verifyregistration.js
+++ verifyregistration.js
@@ -5,12 +5,15 @@ function verifyReg(){
     var validUserTag = userTag.replace("#", "ID");
     if (captchaSuccess == false) {
       alert("recaptcha unverified");
+      console.log("no cap");
     }
     var validTagRegEx = /.*[^# ]ID[0-9]{4}/;
     var validation = validTagRegEx.test(validUserTag);
     if (captchaSuccess == true && validation == true) {
       alert("nice job buddy");
+      console.log("yep");
     } else {
         alert("you done fucked up");
+        console.log("nope");
     }
 }
