4d100f319603e29ba773413b8b548f0359000086
CrookShanks4000 <50306750+CrookShanks4000@users.noreply.github.com>
2020-06-18 15:35:23 -0500

    registration woork

--- verifyregistration.js
+++ verifyregistration.js
@@ -1,7 +1,17 @@
-var captchaResponse = grecaptcha.getResponse();
-if (captchaResponse.success == true) {
-    alert("recaptcha verified");
-
-} else {
-    alert("recaptcha unverified");
-}
+function verifyReg(){
+    var captchaResponse = grecaptcha.getResponse();
+    var captchaSuccess = captchaResponse.success;
+    var userTag = document.getElementById("tag").value;
+    var validUserTag = userTag.replace("#", "ID");
+    if (captchaSuccess == false) {
+      alert("recaptcha unverified");
+    }
+    function validateTag(convertedTag) {
+        const validTagRegEx = /.*[^# ]ID[0-9]{4}/;
+        return validTagRegEx.test(convertedTag);
+    }
+  }
+    var validation = validateTag(validUserTag);
+    if (captchaSuccess == true && validation == true) {
+      alert("nice job buddy");
+    }
